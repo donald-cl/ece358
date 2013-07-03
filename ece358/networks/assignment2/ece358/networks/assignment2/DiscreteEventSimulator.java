7bb711886c750c83e530735655100c224968a7ae
Whodat ninja <seven_up10@hotmail.com>
2013-07-02 22:54:32 -0400

    final shizz

--- ece358/networks/assignment2/ece358/networks/assignment2/DiscreteEventSimulator.java
+++ ece358/networks/assignment2/ece358/networks/assignment2/DiscreteEventSimulator.java
@@ -421,46 +421,11 @@ public class DiscreteEventSimulator {
 	
 	public static void main(String args[]) {
 		
-		//simulate1();
-		//simulate2();
-		//simulate3();
+		simulate1();
+		simulate2();
+		simulate3();
 		simulate4();
 		
-		/*if (args.length == 1 && args[0].equals("-usage")) {
-			debug("------------------------------------------------------------------------");
-			debug("Parameter description:");
-			debug("N: the number of computers connected to the LAN (variable)");
-			debug("A: Data packets arrive at the MAC layer following a Poisson process with an average arrival rate of A packets/second (variable)");
-			debug("W: the speed of the LAN (fixed)");
-			debug("L: packet length (fixed)");
-			debug("P: Persistence parameter for P-persistent CSMA protocols");
-			debug("-----------------------------------------------------------------------");
-			debug("Command Format: ");
-			debug("DiscreteEventSimulator <N> <A> <W> <L> <P>");
-			debug("");
- 		}
- 		else if (args.length == 5) 
- 		{
- 			int compNum = Integer.parseInt(args[0]);
-			int pktArrivalRate = Integer.parseInt(args[1]);
-			int lanSpeed = Integer.parseInt(args[2]);
-			int pktLength = Integer.parseInt(args[3]);
-			int persistanceParam = Integer.parseInt(args[4]);
-			
-			int total = 0;
-			for(int n = 0; n < 5; n++)
-			{
-				fails = 0;
-				isMediumBusy = false;
-				System.out.println("Test #" + n);
-				total = total + driver(compNum, pktArrivalRate, lanSpeed, pktLength, persistanceParam);
-			}
- 			
-			System.out.println("The average is " + total/5);
- 		}
- 		else {
- 			System.out.println("Not enough arguments! Enter DiscreteEventSimulator -usage");
- 		}*/
 	}
 
 	public static void debug(String msg) {
